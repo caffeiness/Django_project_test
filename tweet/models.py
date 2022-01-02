3ce1f3df71261775e70f04014a2b44bb7de0fdfb
caffeiness <caffeine.konchan@gmail.com>
2022-01-02 21:50:50 +0900

    first commit

---
+++ tweet/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import deactivate
+# Create your models here.
+
+class Post(models.Model):
+    content = models.TextField()
+    auther = models.ForeignKey(User,on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.content
+
+
+
