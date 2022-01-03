1f9fb24e4dec6e8bdb8ff558db0a52694c2e4bce
caffeiness <caffeine.konchan@gmail.com>
2022-01-03 23:39:40 +0900

    add frontend

--- tweet/urls.py
+++ tweet/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'tweet'
 
-urlpatterns = {
+urlpatterns = [
     path('',views.PostListView.as_view(),name='index'),
-}
+    path('post/new',views.PostCreateView.as_view(),name='post-create')
+]
--- user/urls.py
+++ user/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'user'
 
-urlpatterns = {
+urlpatterns = [
     path('home/',views.HomeView.as_view(),name='home'),
-}
+]
